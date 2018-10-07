22c2c2d2d4e0e9c40123aa19211d79da394236ba
Elliot Olds <elliotolds@gmail.com>
2018-10-06 22:18:08 -0700

    finish deposit function

--- contracts/Partnership.sol
+++ contracts/Partnership.sol
@@ -2,10 +2,11 @@ pragma solidity ^0.4.0;
 
 contract Partnership{
   // A simplified partnership contract where we assume that
-  // payments are always made monthly.
+  // payments are always made every 4 weeks.
   // Contract is initiated when both people submit the first payment.
 
   public constant uint GRACE_PERIOD_DAYS = 7;
+  public constant uint PAYMENT_PERIOD_DAYS = 28;
 
   public bytes32 agreementHash; 
 
@@ -24,6 +25,15 @@ contract Partnership{
 
   public uint effectiveTime;
   public uint lateFeePercentage;
+
+  public uint nextPaymentDue;
+
+  public bool nextPaymentPaidA;
+  public bool nextPaymentPaidB;
+
+  public bool currentFeeA;
+  public bool currentFeeB;
+
 
   constructor(
       bytes32 _agreementHash,
@@ -44,13 +54,31 @@ contract Partnership{
 
   function deposit() public payable{
     if(msg.sender == partyA){
+      require(msg.value == monthlyPaymentA + currentFeeA && !nextPaymentPaidA);
       amountPaidA += msg.value;
+      if(currentFeeA > 0){
+        partyB.transfer(currentFeeA);
+      }
+      currentFeeA = 0;
+      nextPaymentPaidA = true;
     } else if( msg.sender == partyB){
+      require(msg.value == monthlyPaymentB + currentFeeB && !nextPaymentPaidB);
       amountPaidB += msg.value;
+      if(currentFeeB > 0){
+        partyA.transfer(currentFeeB);
+      }
+      currentFeeB = 0;
+      nextPaymentPaidB = true;
     } 
-    if(effectiveTime == 0 && amountPaidA >= monthlyPaymentA && amountPaidB >= monthlyPaymentB){
+    if(effectiveTime == 0 && nextPaymentPaidA && nextPaymentPaidB){
       effectiveTime = now;
+      nextPaymentDue = effectiveTime + PAYMENT_PERIOD_DAYS;
+      nextPaymentPaidA = false;
+      nextPaymentPaidB = false;
+    } else if( nextPaymentPaidA && nextPaymentPaidB ){
+      nextPaymentDue = nextPaymentDue + PAYMENT_PERIOD_DAYS;
+      nextPaymentPaidA = false;
+      nextPaymentPaidB = false;
     }
   }
-
 }
