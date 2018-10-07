2ba0dd049adba1dd93562e2d118401ad1b31582c
Elliot Olds <elliotolds@gmail.com>
2018-10-06 21:26:45 -0700

    intermediate version of partnership contract

---
+++ contracts/Partnership.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.4.0;
+
+contract Partnership{
+  // A simplified partnership contract where we assume that
+  // payments are always made monthly.
+  // Contract is initiated when both people submit the first payment.
+
+  public constant uint GRACE_PERIOD_DAYS = 7;
+
+  public bytes32 agreementHash; 
+
+  public address partyA;
+  public address partyB;
+  public address arbitrator;
+
+  public uint amountPaidA;
+  public uint amountPaidB;
+
+  public uint monthlyPaymentA;
+  public uint monthlyPaymentB;
+
+  public uint numPaymentsA;
+  public uint numPaymentsB;
+
+  public uint effectiveTime;
+  public uint lateFeePercentage;
+
+  constructor(
+      bytes32 _agreementHash,
+      address _partyA, 
+      address _partyB, 
+      address _arbitrator,
+      uint _monthlyPaymentA,
+      uint _monthlyPaymentB,
+      uint _lateFeePercentage) public {
+    agreementHash = _agreementHash;  
+    partyA = _partyA;
+    partyB = _partyB;
+    arbitrator = _arbitrator;
+    monthlyPaymentA = _monthlyPaymentA;
+    monthlyPaymentB = _monthlyPaymentB;
+    lateFeePercentage = _lateFeePercentage;
+  }
+
+  function deposit() public payable{
+    if(msg.sender == partyA){
+      amountPaidA += msg.value;
+    } else if( msg.sender == partyB){
+      amountPaidB += msg.value;
+    } 
+  }
+
+}
